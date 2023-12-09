4c08ade4c7519df7512218f0815ed9bd0a263e83
9-coding <9.coding.lab@gmail.com>
2023-12-09 21:29:18 +0900

    FEAT: get result path

--- predict.py
+++ predict.py
@@ -1,13 +1,8 @@
 import os
-import cv2
-import numpy as np
-from fastapi import FastAPI, UploadFile, File, HTTPException
-from fastapi.responses import FileResponse
+from fastapi import FastAPI, HTTPException
 from ultralytics import YOLO
-import firebase_admin
 from firebase import uploadFirestore
-from PIL import Image
-import js2py
+import torch
 
 images_path = 'dataset/JPEGImages/'
 annotations_path = 'dataset/Annotations_txt/'
@@ -15,23 +10,46 @@ path = 'dataset/'
 
 app = FastAPI()
 
-model = YOLO("yolov8m.pt")
-i = 33
+# 모델을 yaml 파일 구성에 맞게 초기화
+model = YOLO("yolov8m.yaml")
 
-def add():
-    return i + 1
+# 사전 훈련된 가중치를 로드 (클래스 수가 일치하는지 확인 필요)
+model.load_state_dict(torch.load('yolov8m_trained-15Epoch.pt'))
 
-async def predict_and_return(file_path, i):
-    img = cv2.imread(file_path)
-    model.predict(source=img, conf=0.4, save=True, line_thickness=2)
-    return f"runs/detect/predict29/image0.jpg"
+class_mapping = {
+    0: 'sunglass',
+    1: 'hat',
+    2: 'jacket',
+    3: 'shirt',
+    4: 'pants',
+    5: 'shorts',
+    6: 'skirt',
+    7: 'dress',
+    8: 'bag',
+    9: 'shoe',
+}
+
+# source = ''에 내가 원하는 이미지의 경로를 작성하면된다.
+results = model(source='img.png')
+for result in results:
+    for cls_id, custom_label in class_mapping.items():
+        if cls_id in result.names:  # check if the class id is in the results
+            result.names[cls_id] = custom_label  # replace the class name with the custom label
+
+
+
+# 현재까지는 모델을 예측하면 runs/detect/train#number의 경로로 이미지가 저장된다
+# 저장은 Detection된 이미지, 옷이 crop된 이미지 이렇게 각각의 클래스 폴더가 생성되고 저장된다.
+results = model.predict(source='img.png', conf=0.4, save=True, save_crop= True, line_thickness=2)
+print("path: " + results[0].save_dir)
+# terminal에 저장된 경로가 나오는데 거기로 이동해서 확인해보면 됩니다.
+
 
 import requests
 
 
 @app.post("/detect")
 async def detect(image_link: str):
-    global i
     try:
         print("link", image_link)
         response = requests.get(image_link)
@@ -46,7 +64,8 @@ async def detect(image_link: str):
             with open(img_path, 'wb') as file:
                 file.write(response.content)
 
-            model.predict(source=img_path, conf=0.4, save=True, line_thickness=2)
+            results = model.predict(source=img_path, conf=0.4, save=True, line_thickness=2, project="result", name="result1")
+            print(results[0])
             print(f"이미지 다운로드 완료: {img_name}")
         else:
             print("이미지를 다운로드할 수 없습니다.")
@@ -54,7 +73,7 @@ async def detect(image_link: str):
         response = requests.get(image_link, stream=True)
         response.raise_for_status()
         # 임시 경로
-        predict_path = f"runs/detect/predict{i}/{img_name}"
+        predict_path = f"runs/detect/predict/{img_name}"
         uploadFirestore(predict_path, img_name)
         return {"detail": "Image fetched successfully"}
     except requests.RequestException as e:
